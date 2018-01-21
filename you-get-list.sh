5e6b4a17b063a31fe6e94e9f6bed1eceda5d9df2
dale0110 <dale0110@aliyun.com>
2018-01-21 12:35:58 +0800

    Update you-get-list.sh

--- you-get-list.sh
+++ you-get-list.sh
@@ -1,10 +1,9 @@
 #!/bin/sh
 
-baseUrl="https://www.youtube.com/watch?v=9xizQn-v7V4&list=PLCUd0NAOIRKs2AuqCkHdLOmn8zO1u8nGT&index="
 counter=1
-while [ $counter -lt 111 ]
+for url in `cat down.txt`
   do
-  echo "$baseUrl$counter"
-	you-get -x  127.0.0.1:62543 --itag=36  $baseUrl$counter$
+  echo "$url"
+	you-get -x  127.0.0.1:62543 --itag=36  $url
 	counter=`expr $counter + 1`
 done
