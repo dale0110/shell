1837599d797152a922da8b7ecdbb0af322b354de
dale0110 <4265024+dale0110@users.noreply.github.com>
2023-10-31 11:13:33 +0800

    Update deleteTrashFile.sh

--- deleteTrashFile.sh
+++ deleteTrashFile.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
+function log () {
+
+    echo "[`date +%F-%T`] $1" >> $LOGDIR/platform.log
+}
+
+
 rm -rf /tmp/stdout*
 find /home/ubuntu/.m2/repository/com/zte/sdotn/sdotn-karaf-distribution/ -name "*.tar.gz"|xargs rm -rf
 find /home/ubuntu/.m2/repository/com/zte/sdotn/sdotn-karaf-distribution/ -name "*.zip"|xargs rm -rf
